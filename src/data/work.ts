e63e3c3d47654fa01079f063d07a8c37fef30770
Chris Siku <chrissiku5@gmail.com>
2023-07-18 12:35:57 +0200

    Create works data

---
+++ src/data/work.ts
@@ -0,0 +1,58 @@
+import type { StaticImageData } from "next/image";
+import { v4 as uid } from "uuid";
+import project from "../assets/works/project.jpg";
+
+interface Work {
+  id: string;
+  src: StaticImageData | any | string;
+  title: string;
+  link: string;
+  stack: string[];
+}
+
+const WorksData: Work[] = [
+  {
+    id: uid(),
+    src: project,
+    title: "Burger Builder",
+    link: "https://burger-builder-1a3e5.web.app/",
+    stack: ["React", "Redux", "CSS"],
+  },
+  {
+    id: uid(),
+    src: project,
+    title: "Burger Builder",
+    link: "https://burger-builder-1a3e5.web.app/",
+    stack: ["React", "Redux", "CSS"],
+  },
+  {
+    id: uid(),
+    src: project,
+    title: "Burger Builder",
+    link: "https://burger-builder-1a3e5.web.app/",
+    stack: ["React", "Redux", "CSS"],
+  },
+  {
+    id: uid(),
+    src: project,
+    title: "Burger Builder",
+    link: "https://burger-builder-1a3e5.web.app/",
+    stack: ["React", "Redux", "CSS"],
+  },
+  {
+    id: uid(),
+    src: project,
+    title: "Burger Builder",
+    link: "https://burger-builder-1a3e5.web.app/",
+    stack: ["React", "Redux", "CSS"],
+  },
+  {
+    id: uid(),
+    src: project,
+    title: "Burger Builder",
+    link: "https://burger-builder-1a3e5.web.app/",
+    stack: ["React", "Redux", "CSS"],
+  },
+];
+
+export default WorksData;
