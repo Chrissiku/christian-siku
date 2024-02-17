630d718d1f5dd35733cb0776fa7945aee4c58e59
Chris Siku <chrissiku5@gmail.com>
2024-02-17 13:37:18 +0200

    Add thread icon

--- src/data/socilaMediaIcon.ts
+++ src/data/socilaMediaIcon.ts
@@ -1,4 +1,4 @@
-const baseLink = "https://www.instagram.com/andrewjcameron/";
+import type { StaticImageData } from "next/image";
 import instagramLight from "../assets/social-media/instagram-light.svg";
 import instagramDark from "../assets/social-media/instagram-dark.svg";
 import githubLight from "../assets/social-media/github-light.svg";
@@ -7,10 +7,13 @@ import linkedinLight from "../assets/social-media/linkedin-light.svg";
 import linkedinDark from "../assets/social-media/linkedin-dark.svg";
 import twitterLight from "../assets/social-media/twitter-light.svg";
 import twitterDark from "../assets/social-media/twitter-dark.svg";
+import threadDark from "../assets/social-media/thread-dark.svg";
+import threadLight from "../assets/social-media/thread-light.svg";
 
+const baseLink = "https://www.instagram.com/christian_el_olivero/";
 interface SocialMediaIcon {
   name: string;
-  icons: [any, any]; // Array to store light and dark icon promises
+  icons: [string, string] | StaticImageData; // Array to store light and dark icon promises
   link: string;
 }
 
@@ -21,22 +24,27 @@ interface SocialMediaIcons {
 const socialMediaIcons: SocialMediaIcons = {
   instagram: {
     name: "instagram",
-    icons: [instagramLight, instagramDark],
+    icons: [instagramLight, instagramDark] as [string, string],
     link: baseLink,
   },
   github: {
     name: "github",
-    icons: [githubLight, githubDark],
+    icons: [githubLight, githubDark] as [string, string],
+    link: baseLink,
+  },
+  thread: {
+    name: "thread",
+    icons: [threadLight, threadDark] as [string, string],
     link: baseLink,
   },
   linkedin: {
     name: "linkedin",
-    icons: [linkedinLight, linkedinDark],
+    icons: [linkedinLight, linkedinDark] as [string, string],
     link: baseLink,
   },
   twitter: {
     name: "twitter",
-    icons: [twitterLight, twitterDark],
+    icons: [twitterLight, twitterDark] as [string, string],
     link: baseLink,
   },
 };
